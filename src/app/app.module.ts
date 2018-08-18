d8e3379ffad1ca860d9a730ae159e1d8b073acb4
Julia <utrobinajulia@gmail.com>
2018-08-18 22:36:46 +0300

    Removed default app component. Added joke component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { JokeComponent } from './joke.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    JokeComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [JokeComponent]
 })
 export class AppModule { }
